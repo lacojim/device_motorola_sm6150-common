45a991573131301b04ff3d633f19fc11ab2a8b75
Fraaxius <franthekillaz@gmail.com>
2021-10-08 11:00:27 +0000

    odessa: Drop Lineage things
    * This is AOSP

--- common.mk
+++ common.mk
@@ -29,13 +29,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
-
-PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
-    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
+    $(LOCAL_PATH)/overlay
 
 # A/B
 AB_OTA_UPDATER := true
@@ -332,11 +326,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     ethertypes \
     libebtc
-
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm \
-    vendor.lineage.livedisplay@2.0-service-sysfs.motorola_sm6150
 
 # Media
 PRODUCT_COPY_FILES += \
